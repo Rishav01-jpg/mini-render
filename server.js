eb2b82f3b276d1a77272a4cb742a96a922bf2c54
Rishav01-jpg <kalart496@gmail.com>
2026-05-28 14:58:01 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Mini Render Backend Running");
+});
+
+app.post("/deploy", (req, res) => {
+  const { repoUrl } = req.body;
+
+  console.log("GitHub Repo:", repoUrl);
+
+  res.json({
+    success: true,
+    message: "Deployment started",
+    repoUrl,
+  });
 });
 
 const PORT = 7000;
