d784c126248a31ff7627d00028b82deb86143fd4
Rishav01-jpg <kalart496@gmail.com>
2026-05-29 12:58:09 +0530

    added deployment engine

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const simpleGit = require("simple-git");
+const { exec } = require("child_process");
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
@@ -10,16 +14,66 @@ app.get("/", (req, res) => {
   res.send("Mini Render Backend Running");
 });
 
-app.post("/deploy", (req, res) => {
-  const { repoUrl } = req.body;
+app.post("/deploy", async (req, res) => {
+  try {
+    const { repoUrl } = req.body;
 
-  console.log("GitHub Repo:", repoUrl);
+    if (!repoUrl) {
+      return res.status(400).json({
+        success: false,
+        message: "Repo URL required",
+      });
+    }
 
-  res.json({
-    success: true,
-    message: "Deployment started",
-    repoUrl,
-  });
+    // Project Name
+    const repoName = repoUrl.split("/").pop();
+
+    // Deployment Path
+    const deployPath = path.join(__dirname, "apps", repoName);
+
+    // Create apps folder if not exists
+    if (!fs.existsSync(path.join(__dirname, "apps"))) {
+      fs.mkdirSync(path.join(__dirname, "apps"));
+    }
+
+    // Clone Repo
+    const git = simpleGit();
+
+    await git.clone(repoUrl, deployPath);
+
+    // Install Dependencies
+    exec(`cd ${deployPath} && npm install`, (err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      // Run App
+      exec(
+        `cd ${deployPath} && pm2 start server.js --name ${repoName}`,
+        (err) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+
+          console.log(`${repoName} deployed successfully`);
+        }
+      );
+    });
+
+    res.json({
+      success: true,
+      message: "Deployment started",
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Deployment failed",
+    });
+  }
 });
 
 const PORT = 7000;
