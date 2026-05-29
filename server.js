a9aff559bc355a4b843b7aef19091ca06dff1772
Rishav01-jpg <kalart496@gmail.com>
2026-05-29 13:06:28 +0530

    added frontend link

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const fs = require("fs");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
